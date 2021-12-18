54a1e59ca8a8a27d8eb28172bf8cbc9f35d0d37f
CoderBig <49120922+dogukanc760@users.noreply.github.com>
2021-12-18 14:38:33 +0300

    port listener 2

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("server running");
+const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log('server listening at', address);
 });
